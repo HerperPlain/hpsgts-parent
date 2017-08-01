ea2cb03989968d2402f221c6d2ad01fd71a45080
hpsgts@live.com <Sa123456>
2017-08-02 01:22:32 +0800

    初始化项目整合shiro

--- hpsgts_web/src/main/java/com.hpsgts/crm/user/UserController.java
+++ hpsgts_web/src/main/java/com.hpsgts/crm/user/UserController.java
@@ -3,6 +3,7 @@ package com.hpsgts.crm.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -11,5 +12,9 @@ public class UserController {
     @ResponseBody
     public String say(){
         return "Hello wold";
+    }
+    @RequestMapping(value = "/login")
+    public String login(){
+        return "login";
     }
 }
